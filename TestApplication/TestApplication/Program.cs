f093fbb9c683c7980ee5dcb07a52cb98c19adc18
batbayarb <bbatbayar06@gmail.com>
2018-07-23 22:35:24 +0800

    edit

--- TestApplication/TestApplication/Program.cs
+++ TestApplication/TestApplication/Program.cs
@@ -10,7 +10,13 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("What is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
         }
     }
 }
